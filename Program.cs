c771f9154abf71d83ae0114c896b0b3903019624
Michael Kahan <mjkahan23@gmail.com>
2019-08-24 12:56:45 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleBasketball
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
